ab7b2185ea18c1f082f76c2927b70cdefafcf61a
Jonathan Luk <jluk87@gmail.com>
2018-08-19 19:58:18 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -3,11 +3,10 @@ class Application
   def call
 
     resp = Rack::Response.new
-    
+
     Time.new.hour >= 12 ? resp.write "Good Afternoon!" : resp.finish "Good Morning!"
 
     resp.finish
-    
 
   end
 
